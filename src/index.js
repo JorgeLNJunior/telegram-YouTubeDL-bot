7bdf46f609833ac22176e04e3267cae35e2daef2
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-06-18 14:03:18 -0300

    Retornar innformações básicas sobre o vídeo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 const TelegramBot = require('node-telegram-bot-api')
+const tydl = require('ytdl-core')
+const moment = require('moment')
 const { isYoutubeURL } = require('./utils/link')
 require('dotenv').config()
 
 const token = process.env.TOKEN
 const bot = new TelegramBot(token, { polling: true })
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
   const chatID = msg.chat.id
 
   if (!isYoutubeURL(msg.text)) {
     bot.sendMessage(chatID, 'Por favor, envie um link válido')
   } else {
-    bot.sendMessage(chatID, 'Ainda não disponível')
+    try {
+      bot.sendMessage(chatID, 'Aguarde um pouco...')
+
+      const data = await tydl.getBasicInfo(msg.text)
+
+      const s = moment
+        .utc(moment.duration(data.length_seconds, 'seconds').as('milliseconds'))
+        .format('mm:ss')
+
+      bot.sendMessage(
+        chatID,
+        `📺 *Canal:* ${data.author.name}\n🎬 *Título:* ${data.title}\n🕑 *Duração:* ${s}`,
+        { parse_mode: 'Markdown' }
+      )
+    } catch (error) {
+      bot.sendMessage(chatID, error)
+    }
   }
 })
