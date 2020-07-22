191bdf71ecd4bf1379ffdc11e9d6a768bbae93cc
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-07-22 12:36:20 -0300

    Adicionar opção de escolher a qualidade

--- src/index.js
+++ src/index.js
@@ -7,20 +7,18 @@ require('dotenv').config()
 const { isYoutubeURL, isCommand, secondsToMinutes } = require('./utils')
 
 const TOKEN = process.env.TOKEN
-// const externalURL = 'https://telegram-youtubedl-bot.herokuapp.com/'
-// const HOST = process.env.HOST || '0.0.0.0'
-// const PORT = process.env.PORT || 443
+const externalURL = 'https://telegram-youtubedl-bot.herokuapp.com/'
+const HOST = process.env.HOST || '0.0.0.0'
+const PORT = process.env.PORT || 443
 
-const bot = new TelegramBot(TOKEN, { polling: true })
+const bot = new TelegramBot(TOKEN, {
+  webHook: {
+    port: PORT,
+    host: HOST,
+  },
+})
 
-// const bot = new TelegramBot(TOKEN, {
-//   webHook: {
-//     port: PORT,
-//     host: HOST,
-//   },
-// })
-
-// bot.setWebHook(externalURL + ':' + PORT + '/bot' + TOKEN)
+bot.setWebHook(externalURL + ':' + PORT + '/bot' + TOKEN)
 
 // Commands
 
@@ -59,19 +57,11 @@ bot.on('message', async (msg) => {
     bot.sendMessage(chatID, 'Por favor, envie um link válido')
   } else {
     try {
-      const message = bot.sendMessage(chatID, 'Aguarde um pouco...')
-      const waitGif = bot.sendDocument(
-        chatID,
-        'https://media.giphy.com/media/pFZTlrO0MV6LoWSDXd/giphy.gif'
-      )
       const URL = msg.text
-
       const data = await ytdl.getBasicInfo(URL)
 
       if (data.length_seconds >= 1800) {
-        bot.deleteMessage(chatID, (await message).message_id)
-
-        bot.sendMessage(
+        await bot.sendMessage(
           chatID,
           '*Desculpe, este vídeo não pode ser baixado porque ele ultrapassa os 30 minutos* 😭',
           { parse_mode: 'Markdown' }
@@ -80,70 +70,109 @@ bot.on('message', async (msg) => {
         return
       }
 
-      const videoLength = secondsToMinutes(data.length_seconds)
+      const qualityOptions = []
 
-      const fileName = `${Date.now()}.mp4`
+      data.formats.forEach((label) => {
+        if (
+          label.mimeType.startsWith('video/mp4', 0) &&
+          label.mimeType.includes('avc1') &&
+          !label.qualityLabel.endsWith('60') &&
+          !label.audioQuality
+        ) {
+          qualityOptions.push({
+            text: label.qualityLabel,
+            callback_data: label.itag,
+          })
+        }
+      })
 
-      if (
-        !fs.existsSync('downloads/audio') ||
-        !fs.existsSync('downloads/video') ||
-        !fs.existsSync('downloads/finished')
-      ) {
-        fs.mkdirSync('downloads')
-        fs.mkdirSync('downloads/audio')
-        fs.mkdirSync('downloads/video')
-        fs.mkdirSync('downloads/finished')
-      }
+      const optionsMsg = await bot.sendMessage(
+        chatID,
+        'Selecione a qualidade do vídeo',
+        {
+          reply_markup: {
+            inline_keyboard: [qualityOptions],
+            force_reply: true,
+            remove_keyboard: true,
+          },
+        }
+      )
 
-      ytdl(URL, { quality: 140, filter: 'audio' })
-        .pipe(fs.createWriteStream(`downloads/audio/${fileName}`))
-        .on('error', async (error) => {
-          bot.deleteMessage(chatID, (await message).message_id)
-          bot.deleteMessage(chatID, (await waitGif).message_id)
-          bot.sendMessage(chatID, 'Desculpe, ocorreu um erro')
-          console.log(error)
-          return
-        })
-        .on('finish', async () => {
-          ytdl(URL, { quality: 136, filter: 'video' })
-            .pipe(fs.createWriteStream(`downloads/video/${fileName}`))
-            .on('finish', async () => {
-              ffmpeg()
-                .input(`downloads/video/${fileName}`)
-                .videoCodec('copy')
-                .input(`downloads/audio/${fileName}`)
-                .audioCodec('copy')
-                .save(`downloads/finished/${data.title}.mp4`)
-                .on('error', async (error) => {
-                  bot.deleteMessage(chatID, (await message).message_id)
-                  bot.deleteMessage(chatID, (await waitGif).message_id)
-                  bot.sendMessage(chatID, 'Desculpe, ocorreu um erro')
-                  console.log(error)
-                  return
-                })
-                .on('end', async () => {
-                  await bot.sendVideo(
-                    chatID,
-                    `downloads/finished/${data.title}.mp4`
-                  )
+      bot.on('callback_query', async (query) => {
+        await bot.deleteMessage(chatID, optionsMsg.message_id)
+        const response = query.data
 
-                  bot.deleteMessage(chatID, (await message).message_id)
-                  bot.deleteMessage(chatID, (await waitGif).message_id)
+        const message = await bot.sendMessage(chatID, 'Aguarde um pouco...')
+        const waitGif = await bot.sendDocument(
+          chatID,
+          'https://media.giphy.com/media/pFZTlrO0MV6LoWSDXd/giphy.gif'
+        )
 
-                  bot.sendMessage(
-                    chatID,
-                    `📺 *Canal:* ${data.author.name}\n🎬 *Título:* ${data.title}\n🕑 *Duração:* ${videoLength}`,
-                    { parse_mode: 'Markdown' }
-                  )
+        const videoLength = secondsToMinutes(data.length_seconds)
 
-                  fs.unlinkSync(`downloads/video/${fileName}`)
-                  fs.unlinkSync(`downloads/audio/${fileName}`)
-                  fs.unlinkSync(`downloads/finished/${data.title}.mp4`)
-                })
-            })
-        })
+        const fileName = `${Date.now()}.mp4`
+
+        if (
+          !fs.existsSync('downloads/audio') ||
+          !fs.existsSync('downloads/video') ||
+          !fs.existsSync('downloads/finished')
+        ) {
+          fs.mkdirSync('downloads')
+          fs.mkdirSync('downloads/audio')
+          fs.mkdirSync('downloads/video')
+          fs.mkdirSync('downloads/finished')
+        }
+
+        ytdl(URL, { quality: 140, filter: 'audio' })
+          .pipe(fs.createWriteStream(`downloads/audio/${fileName}`))
+          .on('error', async (error) => {
+            await bot.deleteMessage(chatID, message.message_id)
+            await bot.deleteMessage(chatID, waitGif.message_id)
+            await bot.sendMessage(chatID, 'Desculpe, ocorreu um erro')
+            console.log(error)
+            return
+          })
+          .on('finish', async () => {
+            ytdl(URL, { quality: response, filter: 'video' })
+              .pipe(fs.createWriteStream(`downloads/video/${fileName}`))
+              .on('finish', async () => {
+                ffmpeg()
+                  .input(`downloads/video/${fileName}`)
+                  .videoCodec('copy')
+                  .input(`downloads/audio/${fileName}`)
+                  .audioCodec('copy')
+                  .save(`downloads/finished/${data.title}.mp4`)
+                  .on('error', async (error) => {
+                    await bot.deleteMessage(chatID, message.message_id)
+                    await bot.deleteMessage(chatID, waitGif.message_id)
+                    await bot.sendMessage(chatID, 'Desculpe, ocorreu um erro')
+                    console.log(error)
+                    return
+                  })
+                  .on('end', async () => {
+                    await bot.sendVideo(
+                      chatID,
+                      `downloads/finished/${data.title}.mp4`
+                    )
+
+                    await bot.deleteMessage(chatID, message.message_id)
+                    await bot.deleteMessage(chatID, waitGif.message_id)
+
+                    bot.sendMessage(
+                      chatID,
+                      `📺 *Canal:* ${data.author.name}\n🎬 *Título:* ${data.title}\n🕑 *Duração:* ${videoLength}`,
+                      { parse_mode: 'Markdown' }
+                    )
+
+                    fs.unlinkSync(`downloads/video/${fileName}`)
+                    fs.unlinkSync(`downloads/audio/${fileName}`)
+                    fs.unlinkSync(`downloads/finished/${data.title}.mp4`)
+                  })
+              })
+          })
+      })
     } catch (error) {
-      bot.sendMessage(chatID, 'Desculpe, ocorreu um erro')
+      await bot.sendMessage(chatID, 'Desculpe, ocorreu um erro')
       return
     }
   }
